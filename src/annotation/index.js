31170c679a7028b5cf9a0a5c85102a5aa603cbbf
Yuanjianxin <1814888800@qq.com>
2018-05-30 17:04:28 +0800

    add decorator example

---
+++ src/annotation/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by yuanjianxin on 2018/5/29.
+ */
+import Test from "./Test";
+
+export {
+    Test
+}
--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -1,11 +1,12 @@
 /**
  * Created by yuanjianxin on 2018/4/26.
  */
+import {Test} from "../annotation";
 const BaseController=require('./BaseController');
 module.exports=class TestController extends BaseController{
 
 
-
+    @Test
     async test(ctx,next){
         ctx.logger.info('==hello test==');
 
